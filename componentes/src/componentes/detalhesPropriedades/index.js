0e6f2b5f08a4ff9506cdaf1830582d86cb2f0e11
Gabriel <nobrecoder@gmail.com>
2022-11-30 15:18:13 -0300

    30/11/2022

--- componentes/src/componentes/detalhesPropriedades/index.js
+++ componentes/src/componentes/detalhesPropriedades/index.js
@@ -1,10 +1,18 @@
 import React from "react";
 import { SafeAreaView, Text } from "react-native";
 
+/* PROPRIEDADES SÃO SOMENTE LEITURA:
+
+    Cuidado propriedades são somente leitura, elas não guardam valores fixos quando chegam aos componentes, e se você mexer nelas diretamente, elas vão mudar o valor.
+
+    É sempre mais seguro criar uma variável para que o valor recebido possa ser trabalhado, do que tentar manipular a propriedade diretamente.
+
+ */
+
 export default (props) => {
-    numeroTrocado = props.numero
-    numeroTrocado = 10
-    props.numero = 10
+    numeroTrocado = props.numero //Veja que não guarda valor na memória...
+    numeroTrocado = 10 //Trocamos esse valor e só vai referenciar a esta variável...
+    props.numero = props.numero + 10 //Porém se trocarmos o valor da propriedade diretamente, ela vai mudar o valor da propriedade.
     return(
         <SafeAreaView>
             <Text>
