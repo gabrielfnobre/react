b38d35a0d492f6e7ea6ecccceba21977d8472bd2
Gabriel-F-Nobre <nobrecoder@gmail.com>
2022-12-23 07:35:44 -0300

    23/12/2022

--- componentes/src/navegacao/navigation/index.js
+++ componentes/src/navegacao/navigation/index.js
@@ -3,6 +3,14 @@ import { SafeAreaView, StyleSheet, Text } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import Stack from "./Stack";
 
+/*  REACT NAVIGATION:
+
+    Como navegar de uma tela para a outra em aplicativos React-Native?
+    A grande resposta é o React-Navigation uma Biblioteca React que pode ser baixada na nossa aplicação, que traz uma série de modelos para navegar 
+
+
+ */
+
 export default props => {
     return (
         <SafeAreaView style={styles.safeArea}>
