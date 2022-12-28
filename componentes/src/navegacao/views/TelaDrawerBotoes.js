4889b71c8142b3616deda9ae8c31422db60920b4
Gabriel-F-Nobre <nobrecoder@gmail.com>
2022-12-28 12:30:11 -0300

    28/12/2022

--- componentes/src/navegacao/views/TelaDrawerBotoes.js
+++ componentes/src/navegacao/views/TelaDrawerBotoes.js
@@ -2,6 +2,15 @@ import React from "react";
 import { Button, StyleSheet, View } from "react-native";
 import TextoCentral from "../componentes/TextoCentral";
 
+/*  MÉTODOS OPENDRAWER CLOSEDRAWER E TOGGLEDRAWER DO NAVIGATION DRAWER:
+
+    Na biblioteca drawer da react-navigation, temos alguns métodos que podem ser usados para abri um drawer
+    navigation, fechá-lo e até alterná-lo.
+
+    Vejamos alguns exemplos logo abaixo...
+
+*/
+
 export default props => {
     return (
         <View style={styles.wrapperFora}>
@@ -9,10 +18,14 @@ export default props => {
                 <Button 
                     title="Abrir e Alternar" 
                     onPress={() => {
+                        //Note que estamos usando openDrawer para abrir o drawer através de um botão...
                         props.navigation.openDrawer()
                         setTimeout(() => {
+                            //Usamos closeDrawer para fechar o drawer automáticamente depois de 3 segundos...
                             props.navigation.closeDrawer()
                             setInterval(() => {
+                                /*E agora usamos o toggleDrawer para deixar o drawer louco, alternando entre
+                                    fechado e aberto de 1 em 1 segundo...*/
                                 props.navigation.toggleDrawer()
                             }, 1000)
                         }, 3000)
